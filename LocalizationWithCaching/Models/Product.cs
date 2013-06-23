d02d1e558c24f4c10a51a3f9cd74cda9d941330e
Michael Buen <i.enable.much@gmail.com>
2013-06-23 17:58:06 +0800

    removed stale code (need to learn how to merge)

--- LocalizationWithCaching/Models/Product.cs
+++ LocalizationWithCaching/Models/Product.cs
@@ -8,8 +8,6 @@ namespace LocalizationWithCaching.Models
     public class Product
     {
         public virtual int ProductId { get; set; }
-        public virtual int YearIntroduced { get; set; }
-
-        // public virtual IList<ProductLanguage> ProductLanguage { get; set; } // To instance name is singular, to emphasize we are always getting one element
+        public virtual int YearIntroduced { get; set; }        
     }
 }
